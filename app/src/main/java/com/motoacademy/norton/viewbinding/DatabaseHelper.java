b9f24c61a2dd5efe4aa586e67e4c95c34f476f63
NortonLiaz88 <vnla.lic18@uea.edu.br>
2023-04-24 20:28:07 -0400

    feat: implements Person Db actions

--- app/src/main/java/com/motoacademy/norton/viewbinding/DatabaseHelper.java
+++ app/src/main/java/com/motoacademy/norton/viewbinding/DatabaseHelper.java
@@ -1,15 +1,16 @@
 package com.motoacademy.norton.viewbinding;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import com.motoacademy.norton.viewbinding.domain.Person;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static DatabaseHelper sInstance;
-
-    private Context context;
-
     public static synchronized DatabaseHelper getInstance(Context context) {
         // Use the application context, which will ensure that you
         // don't accidentally leak an Activity's context.
@@ -28,9 +29,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "person.db";
     private static final String TABLE_NAME = "tbl_names";
     private static final String COLUMN_ID = "id";
+    private static final String COLUMN_USERID = "userId";
     private static final String COLUMN_FNAME = "fname";
     private static final String COLUMN_LNAME = "Lname";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
 
     @Override
     public void onConfigure(SQLiteDatabase db) {
@@ -42,9 +44,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String CREATE_NAMES_TABLE = "CREATE TABLE " + TABLE_NAME +
                 "(" +
-                COLUMN_ID + " INTEGER PRIMARY KEY," +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 COLUMN_FNAME + " TEXT," +
-                COLUMN_LNAME + " TEXT" +
+                COLUMN_LNAME + " TEXT," +
+                COLUMN_USERID + "TEXT" +
                 ")";
 
         sqLiteDatabase.execSQL(CREATE_NAMES_TABLE);
@@ -57,6 +60,74 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
             onCreate(sqLiteDatabase);
         }
+    }
 
+    public void addPerson(Person person) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            ContentValues  values = new ContentValues();
+            values.put(COLUMN_FNAME, person.getFirstName());
+            values.put(COLUMN_LNAME, person.getLastName());
+            values.put(COLUMN_USERID, person.getUserId());
+
+            int userAlreadyExist = verifyPersonAlreadyExists(person.getUserId());
+            if(userAlreadyExist > -1) {
+                db.update(TABLE_NAME, values, COLUMN_USERID + " = " + person.getUserId(), null);
+            } else {
+                db.insertOrThrow(TABLE_NAME, null, values);
+            }
+            db.setTransactionSuccessful();
+        }catch (Exception e) {
+            Log.d("DB ERROR", "Error while trying to add person");
+        } finally {
+            db.endTransaction();
+        }
+    }
+
+    public int verifyPersonAlreadyExists(String id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME+" WHERE " + COLUMN_USERID + " = " + id + "", null);
+            if(cursor != null) {
+                cursor.moveToFirst();
+                return cursor.getColumnIndex(COLUMN_USERID);
+            }
+            return -1;
+        }catch (Exception e) {
+            Log.d("DB ERROR", "Error while trying verifying person");
+        } finally {
+            db.endTransaction();
+        }
+        return -1;
+    }
+
+    public Person fetchPerson(String id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try {
+            Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME+" WHERE " + COLUMN_USERID + " = " + id + "", null);
+            if(cursor != null) {
+                cursor.moveToFirst();
+                int userIdIndex = cursor.getColumnIndex(COLUMN_USERID);
+                String userId = cursor.getString(userIdIndex);
+
+                int nameIndex = cursor.getColumnIndexOrThrow(COLUMN_FNAME);
+                String firstName = cursor.getString(nameIndex);
+
+                int lastNameIndex = cursor.getColumnIndexOrThrow(COLUMN_LNAME);
+                String lastName = cursor.getString(lastNameIndex);
+
+                Person person = new Person(firstName, lastName, id);
+                return person;
+            }
+            return null;
+        }catch (Exception e) {
+            Log.d("DB ERROR", "Error while trying to add person");
+        } finally {
+            db.endTransaction();
+        }
+        return null;
     }
 }
--- app/src/main/java/com/motoacademy/norton/viewbinding/FormActivity.java
+++ app/src/main/java/com/motoacademy/norton/viewbinding/FormActivity.java
@@ -3,12 +3,73 @@ package com.motoacademy.norton.viewbinding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.motoacademy.norton.viewbinding.databinding.ActivityFormBinding;
+import com.motoacademy.norton.viewbinding.databinding.ActivityHomeBinding;
+import com.motoacademy.norton.viewbinding.domain.Person;
 
 public class FormActivity extends AppCompatActivity {
+
+    private ActivityFormBinding binding;
+    private FirebaseAuth mAuth;
+
+    private DatabaseHelper db;
+    FirebaseUser currentUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form);
+        binding = ActivityFormBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        db = DatabaseHelper.getInstance(FormActivity.this);
+        setupSaveButton();
+    }
+
+    private void setupSaveButton() {
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String firstName, lastName;
+                firstName = binding.inputFirstName.getText().toString();
+                lastName = binding.inputLastName.getText().toString();
+                if(TextUtils.isEmpty(firstName)) {
+                    Toast.makeText(FormActivity.this, "O primeiro nome não pode estar vazio.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(lastName)){
+                    Toast.makeText(FormActivity.this, "O segundo nome não pode estar vazia.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                try{
+                    Person person = new Person(firstName, lastName, currentUser.getUid());
+                    db.addPerson(person);
+                } catch (Exception e) {
+                    Toast.makeText(FormActivity.this, "Não foi possível salvar os dados", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void showPerson() {
+        binding.btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Person person = db.fetchPerson(currentUser.getUid());
+                binding.inputFirstName.setText(person.getFirstName());
+                binding.inputLastName.setText(person.getLastName());
+            }
+        });
+
     }
 }
